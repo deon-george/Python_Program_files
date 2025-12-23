ec8f2298a68dba6198b1d1080a13ff88bbce5b0f
Deon George <163717656+deon-george@users.noreply.github.com>
2025-12-23 23:51:00 +0530

    Add files via upload

---
+++ min_desk problem.py
@@ -0,0 +1,11 @@
+# Read number of students in three classes
+a = int(input())
+b = int(input())
+c = int(input())
+
+# Calculate desks needed
+desks = (a + 1) // 2 + (b + 1) // 2 + (c + 1) // 2
+
+# Print the result
+print(desks)
+
