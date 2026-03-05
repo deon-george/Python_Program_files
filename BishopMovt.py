eb5bd4565f65cff484faad6bebbf89814bb1e1a3
deon george <deongeorgevadakkel@gmail.com>
2026-03-05 15:57:46 +0530

    adding problem of bishop movement and factorial calculation

--- BishopMovt.py
+++ BishopMovt.py
@@ -1,19 +1,21 @@
 print('enter the first cell co-ordinates')
 a=int(input())
 b=int(input())
-if a<0 and b <0:
+if not (1 <= a <= 8 and 1 <= b <= 8):
     print('Error, enter a valid co-oridnate')
     exit()
-if a>=8 and b >=8:
-   print('Error, enter a valid co-oridnate')
-   exit()
-     
+
 print('enter the second cell co-ordinates')
 c=int(input())
 d=int(input())
-if c and d <0:
+if not (1 <= c <= 8 and 1 <= d <= 8):
     print('Error, enter a valid co-oridnate')
     exit()
-if c and d >8:
-    print('Error, enter a valid co-oridnate')
-    exit()
+
+#movt logic
+if  (a-b)%2==0 and (c-d)%2==0:
+    print('Black Bishop Movt is possible')
+elif ((a-b)+1)%2==0 and ((c-d)+1)%2==0:
+    print('White Bishop Movt is possible')
+else:
+    print('Bishop Movt is not possibe')
---
+++ factorial.py
@@ -0,0 +1,8 @@
+print("enter the value of n")
+n=int(input())
+fact=1
+print(f'Factorial of {n}=')
+for i in range(1,n+1):
+ fact=fact*i
+print(fact)
+ 
