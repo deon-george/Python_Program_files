471d3b5f6f6311146577c50ba7113f48df4d008c
deon george <deongeorgevadakkel@gmail.com>
2026-05-21 13:02:15 +0530

    love.py

--- Ladder.py
+++ Ladder.py
@@ -3,11 +3,13 @@ n = int(input())
 if n>9:
     exit()
 else:
-    for i in range(1,n+1):
-        print(i)
-        print('\n')
-        for j in range(i):
-            print(j)
-            print('\n')
-            for k in range(1):
-              print(k)
+     print('\n')
+    #printing the rows
+    #very imp line to navigate to next line
+     print()
+     for i in range(1, n+1):
+      #printing numbers
+      for j in range(1, i+1):
+        
+        print(j,end="")
+      print()
