8a13c1baa5616f13bc9bda69786ecfb690af9214
deon george <deongeorgevadakkel@gmail.com>
2026-06-20 23:28:52 +0530

    correct the code in carProblem

--- carProblem.py
+++ carProblem.py
@@ -1,10 +1,5 @@
-print ('we are considering the car covers n km in 1 day')
-print ('\n')
-
-print('enter the n distance')
-n= int (input())
-print('enter the m distance')
-m= int (input())
-
-days= m/n *1
-print (f'number of days needed to cover {m} km is', days)
+import math
+n = int(input())  # km per day
+m = int(input())  # total distance
+days = math.ceil(m / n)
+print(days)
