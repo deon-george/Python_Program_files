27011909f7ef574d07270ae14319897353285bc5
deon george <deongeorgevadakkel@gmail.com>
2026-06-20 23:14:56 +0530

    snaikfy problems

---
+++ deskProblem.py
@@ -0,0 +1,9 @@
+import math
+a=int(input())
+b=int(input())
+c=int(input())
+a1=math.ceil(a/2)
+b1=math.ceil(b/2)
+c1=math.ceil(c/2)
+total=a1+b1+c1
+#print(total)
---
+++ wordCount.py
@@ -0,0 +1,8 @@
+# Read an integer:
+# a = int(input())
+# Read a float:
+# b = float(input())
+# Print a value:
+# print(a, b)
+a=input()
+print(a.count(" ")+1)
